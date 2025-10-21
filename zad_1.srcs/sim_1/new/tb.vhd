c8f6ef912832cf1b3793b4c294ded5c2feca8970
trebuHE <47220041+trebuHE@users.noreply.github.com>
2025-10-21 21:41:31 +0200

    initial

---
+++ zad_1.srcs/sim_1/new/tb.vhd
@@ -0,0 +1,54 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity tb is
+--  Port ( );
+end tb;
+
+architecture Behavioral of tb is
+    component top is
+        Port ( CLK100MHZ : in STD_LOGIC;
+               LED : out STD_LOGIC_VECTOR (15 downto 0);
+               AD_MCLK : out STD_LOGIC;
+               AD_LRCK : out STD_LOGIC;
+               AD_SCLK : out STD_LOGIC;
+               AD_SDOUT : in STD_LOGIC;
+               DA_MCLK : out STD_LOGIC;
+               DA_LRCK : out STD_LOGIC;
+               DA_SCLK : out STD_LOGIC;
+               DA_SDIN : out STD_LOGIC);
+    end component top;
+    
+    signal clk : std_logic := '0';
+    signal led : std_logic_vector(15 downto 0) := (others => '0');
+    signal ad_mclk : std_logic := '0';
+    signal ad_lrck : std_logic := '0';
+    signal ad_sclk : std_logic := '0';
+    signal ad_sdout : std_logic := '0';
+    signal da_mclk : std_logic := '0';
+    signal da_lrck : std_logic := '0';
+    signal da_sclk : std_logic := '0';
+    signal da_sdin : std_logic := '0';
+begin
+
+dut: top port map(CLK100MHZ => clk, LED => led, AD_MCLK => ad_mclk, AD_LRCK => ad_lrck, AD_SCLK => ad_sclk, AD_SDOUT => ad_sdout, DA_MCLK => da_mclk, DA_LRCK => da_lrck, DA_SCLK => da_sclk, DA_SDIN => da_sdin);
+
+clock : process
+    constant clk_T : time := 10ns;
+begin
+    clk <= '1';
+    wait for clk_T/2;
+    clk <= '0';
+    wait for clk_T/2;
+end process;
+
+stim : process
+    constant sclk_T : time := 320ns;
+begin
+    ad_sdout <= '1';
+    wait for sclk_T;
+    ad_sdout <= '0';
+    wait for sclk_T;
+end process;
+end Behavioral;
---
+++ zad_1.srcs/sources_1/new/top.vhd
@@ -0,0 +1,88 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           LED : out STD_LOGIC_VECTOR (15 downto 0);
+           AD_MCLK : out STD_LOGIC;
+           AD_LRCK : out STD_LOGIC;
+           AD_SCLK : out STD_LOGIC;
+           AD_SDOUT : in STD_LOGIC;
+           DA_MCLK : out STD_LOGIC;
+           DA_LRCK : out STD_LOGIC;
+           DA_SCLK : out STD_LOGIC;
+           DA_SDIN : out STD_LOGIC);
+end top;
+
+architecture Behavioral of top is
+    signal clk_counter : unsigned(10 downto 0) := (others => '0');
+    
+    signal sclk_prev : std_logic := '0';
+    signal lrck_prev : std_logic := '0';
+    
+    signal in_left : std_logic_vector(23 downto 0) := (others => '0');
+    signal in_right : std_logic_vector(23 downto 0) := (others => '0');
+    signal out_left : std_logic_vector(23 downto 0) := (others => '0');
+    signal out_right : std_logic_vector(23 downto 0) := (others => '0');
+    
+    signal strobe : std_logic := '0';
+    
+    signal led_abs : std_logic_vector(15 downto 0) := (others => '0');
+begin
+
+    process(CLK100MHZ)
+    begin
+        if rising_edge(CLK100MHZ) then
+            clk_counter <= clk_counter + 1;
+        end if;     
+    end process;
+    
+    process(CLK100MHZ)
+    begin
+        if rising_edge(CLK100MHZ) then
+            strobe <= '0';
+            sclk_prev <= clk_counter(4);
+            lrck_prev <= clk_counter(10);
+            
+            if clk_counter(10) = '0' then
+                if sclk_prev = '0' and clk_counter(4) = '1' then
+                    in_left <= in_left(22 downto 0) & AD_SDOUT;
+                elsif sclk_prev = '1' and clk_counter(4) = '0' then
+                    DA_SDIN <= out_left(23);
+                    out_left <= out_left(22 downto 0) & '0';
+                end if;
+            elsif clk_counter(10) = '1' then
+                if sclk_prev = '0' and clk_counter(4) = '1' then
+                    in_right <= in_right(22 downto 0) & AD_SDOUT;
+                elsif sclk_prev = '1' and clk_counter(4) = '0' then
+                    DA_SDIN <= out_right(23);
+                    out_right <= out_right(22 downto 0) & '0';
+                end if;
+            end if;
+            
+            if lrck_prev = '1' and clk_counter(10) = '0' then
+                out_left <= in_left;
+                out_right <= in_right;
+                in_left <= (others => '0');
+                in_right <= (others => '0');
+                strobe <= '1';
+            end if;
+            
+        end if;
+    end process;
+    
+    process(strobe)
+    begin
+    
+    end process;
+
+    AD_MCLK <= clk_counter(2);
+    DA_MCLK <= clk_counter(2);
+    
+    AD_SCLK <= clk_counter(4);
+    DA_SCLK <= clk_counter(4);
+    
+    AD_LRCK <= clk_counter(10);
+    DA_LRCK <= clk_counter(10);
+end Behavioral;
