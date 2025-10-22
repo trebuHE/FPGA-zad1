e647217dbaae495e56f43796369720976a0b343e
Hubert Mucha <47220041+trebuHE@users.noreply.github.com>
2025-10-22 10:52:17 +0200

    Add files via upload

--- zad_1.srcs/sources_1/new/top.vhd
+++ zad_1.srcs/sources_1/new/top.vhd
@@ -28,7 +28,7 @@ architecture Behavioral of top is
     
     signal strobe : std_logic := '0';
     
-    signal led_abs : std_logic_vector(15 downto 0) := (others => '0');
+    signal led_abs : std_logic_vector(15 downto 0) := (others => '1');
 begin
 
     process(CLK100MHZ)
@@ -74,8 +74,12 @@ begin
     
     process(strobe)
     begin
-    
+        if falling_edge(strobe) then
+            led_abs <= std_logic_vector(abs(signed(out_left(23 downto 8))));
+        end if;
     end process;
+
+    LED <= led_abs;
 
     AD_MCLK <= clk_counter(2);
     DA_MCLK <= clk_counter(2);
